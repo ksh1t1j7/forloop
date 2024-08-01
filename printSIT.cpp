9482e81976cc491719f0d175f2b5578f359b49da
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-08-01 12:03:01 +0530

    Update printSIT.cpp

---
+++ printSIT.cpp
@@ -0,0 +1,16 @@
+/*kshitij yadav 23070123075*/
+#include <iostream>
+using namespace std;
+int main() {
+    for (int i = 0; i < 5; ++i) {
+        cout << "SIT" << endl;
+    }
+    return 0;
+}
+/*
+SIT
+SIT
+SIT
+SIT
+SIT
+*/
