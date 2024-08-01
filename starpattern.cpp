49b57dc71e2c569c1bbedb9d760eb622640fcfe9
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-08-01 12:05:08 +0530

    Update starpattern.cpp

---
+++ starpattern.cpp
@@ -0,0 +1,21 @@
+/*kshitij yadav 23070123075*/
+#include <iostream>
+using namespace std;
+
+int main() {
+    int i, j;
+    for (int i = 1; i <= 5; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
+/*
+* 
+* * 
+* * * 
+* * * * 
+* * * * * 
+*/
