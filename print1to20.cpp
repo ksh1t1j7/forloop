03e9621080fdf6ac67b971f491fd77b7e7bf99aa
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-08-08 11:38:47 +0530

    Create print1to20.cpp

---
+++ print1to20.cpp
@@ -0,0 +1,38 @@
+//23070123075 kshitij yadav
+#include <iostream>
+using namespace std;
+
+int main() {
+    int number = 1; 
+    
+    while (number <= 20) { 
+        cout << number << endl; 
+        number++; 
+    }
+    return 0;
+}
+/*
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+
+
+=== Code Execution Successful ===
+*/
