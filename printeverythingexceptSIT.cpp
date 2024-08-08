edeb1a72a9d661dc3beea66ba554351f505a0d3d
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-08-08 11:46:12 +0530

    Create printeverythingexceptSIT.cpp

---
+++ printeverythingexceptSIT.cpp
@@ -0,0 +1,22 @@
+// Kshitij yadav 23070123075
+#include <iostream>
+using namespace std;
+
+int main() {
+    string k;
+    cin>>k;
+    do{
+        cout<<k<<"\n";
+        cin>>k;
+    }
+    while(k!="sit");
+}
+/*
+hey
+hey
+
+sit
+
+
+=== Code Execution Successful ===
+*/
