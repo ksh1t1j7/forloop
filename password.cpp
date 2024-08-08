a64f432877bd17e2d992d11f36cac80c8d7a7882
ksh1t1j7 <chess.comkshitij@gmail.com>
2024-08-08 11:40:58 +0530

    Create password.cpp

---
+++ password.cpp
@@ -0,0 +1,25 @@
+//kshitij yadav 23070123075
+#include<iostream>
+#include<string>
+
+using namespace std;
+int main()
+{
+    int password, cor_password=1234;
+    cout<<"Enter password: "<<endl;
+    cin>>password;
+    while(password>0)
+    if(password==cor_password)
+    {
+        cout<<"welcome";
+        break;
+    
+    }
+}
+/*
+Enter password: 
+1234
+welcome
+
+=== Code Execution Successful ===
+*/
